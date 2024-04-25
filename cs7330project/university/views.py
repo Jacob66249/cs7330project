d6edb27565ed8f205fbdde13e73e4dfbd7d6fe0f
Jacob66249 <394039547@qq.com>
2024-04-25 00:45:44 -0500

    course function and webpage design

--- cs7330project/university/views.py
+++ cs7330project/university/views.py
@@ -11,12 +11,12 @@ def home(request):
 # Course
 def list_course(request):
     queryset = models.Course.objects.all().order_by("course_id")
-    return render(request, "university/course/course.html", {"queryset": queryset})
+    return render(request, "course/course_list.html", {"queryset": queryset})
 
 
 def add_course(request):
     if request.method == "GET":
-        return render(request, "university/course/add_course.html")
+        return render(request, "course/add_course.html")
     course_Id = request.POST.get("course_id")
     name = request.POST.get("name")
     models.Course.objects.create(course_id=course_Id, name=name)
@@ -29,13 +29,11 @@ def delete_course(request):
     return redirect("/course/")
 
 
-def edit_course(request, course_Id):
+def edit_course(request, Course_Id):
     if request.method == "GET":
-        row_object = models.Course.objects.filter(course_id=course_Id).first()
-        return render(
-            request, "university/course/edit_course.html", {"row_object": row_object}
-        )
+        row_object = models.Course.objects.filter(course_id=Course_Id).first()
+        return render(request, "course/edit_course.html", {"row_object": row_object})
 
-    name = request.POST.get("name")
-    models.Course.objects.filter(course_id=course_Id).update(name=name)
+    Name = request.POST.get("name")
+    models.Course.objects.filter(course_id=Course_Id).update(name=Name)
     return redirect("/course/")
